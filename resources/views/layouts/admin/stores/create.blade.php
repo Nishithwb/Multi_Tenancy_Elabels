1f8550a96babbac5995df60d5557343f5967cae6
Nishithwb <nishith@webbrainstechnologies.in>
2025-09-17 12:27:06 +0530

    test1

--- resources/views/layouts/admin/stores/create.blade.php
+++ resources/views/layouts/admin/stores/create.blade.php
@@ -9,6 +9,7 @@
             <label>Store Name</label>
             <input type="text" name="name" class="form-control" required>
         </div>
+        
         <div class="mb-3">
             <label class="form-label">POS Type</label>
             <select name="pos_type" id="pos_type" class="form-select" required>
